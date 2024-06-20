791756a82b34cc7f5513705deec0fc40360ea6af
Nathan Maciel <112430114+nathanmacielviana@users.noreply.github.com>
2024-06-20 17:31:56 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Login from "./Components/Login/Login.js";
+import "./Components/Login/Login.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import { useState } from "react";
+import "./Login.css";
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Envio");
+  };
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Login System</h1>
+        <div className="input-field">
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembrar de mim?
+            <a href="#">Esqueci a senha </a>
+          </label>
+        </div>
+        <button>Entrar</button>
+
+        <div className="signup-link">
+          <p>Não tem uma conta? </p>
+          <a href="#">Registrar</a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
